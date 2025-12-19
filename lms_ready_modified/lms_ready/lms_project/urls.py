b5a8ee8d652680dc14c743e6af222fe399b804d6
Pelmen04000 <159651085+Pelmen04000@users.noreply.github.com>
2025-12-19 16:41:34 +0100

    Fix indentation for login path in urls.py

--- lms_ready_modified/lms_ready/lms_project/urls.py
+++ lms_ready_modified/lms_ready/lms_project/urls.py
@@ -7,10 +7,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('login/', auth_views.LoginView.as_view(
-        template_name='auth/login.html'), name='login'),
+        template_name='auth/login.html'
+    ), name='login'),
 
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
     path('register/', course_views.register, name='register'),
 
     path('', include('courses.urls')),
